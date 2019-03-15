119fa4331742a057cd4212440fb55bd1cbef6806
kelvingao <89156201@qq.com>
2019-03-15 16:27:14 +0800

    Strings - Working with Textual Data

---
+++ 02_01/textual.py
@@ -0,0 +1,48 @@
+
+# print welcome message
+message = 'Hello World'
+
+
+# escape character
+# message = 'Bobby\'s World'
+
+
+# multi line message
+# message = """Bobby's World was a good
+# cartoon in the 1990s"""
+
+
+# length of the message
+# print(len(message))
+# print(message[0])
+# print(message[0:5])
+# print(message[:5])
+# print(message[6:])
+
+
+# string method
+# print(message.lower())
+# print(message.upper())
+# print(message.count('l'))
+# print(message.find('l'))
+
+
+# replacement
+# message = message.replace('World', 'Universe')
+
+
+# format string
+# greeting = 'Hello'
+# name = 'Michael'
+# message = greeting + ', ' + name + '. Welcome!'
+# message = '{}, {}. Welcome!'.format(greeting, name)
+# message = f'{greeting}, {name.upper()}. Welcome!'
+
+
+print(message)
+
+
+# string all methods
+# print(dir(str))
+# print(help(str))
+# print(help(str.lower))
