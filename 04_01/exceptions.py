e5b4e36cf2b21c01c99e060aa78f5edd14b0caba
kelvingao <89156201@qq.com>
2019-03-25 12:02:26 +0800

    Using Try/Except Blocks for Error Handling

---
+++ 04_01/exceptions.py
@@ -0,0 +1,15 @@
+
+try:
+    # f = open('test_file.txt')
+    f = open('corrupt_file.txt')
+    # if f.name == 'corrupt_file.txt':
+    #     raise Exception
+except FileNotFoundError as e:
+    print(e)
+except Exception as e:
+    print(e)
+else:
+    print(f.read())
+    f.close()
+finally:
+    print("Excuting Finally...")
