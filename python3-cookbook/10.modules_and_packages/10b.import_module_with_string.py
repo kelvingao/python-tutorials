13c2e49651ef2921130b16fe5ca50a3a18f2a914
Kelvin Gao <89156201@qq.com>
2020-11-09 10:21:50 +0800

    cookbook: import module with string code

---
+++ python3-cookbook/10.modules_and_packages/10b.import_module_with_string.py
@@ -0,0 +1,16 @@
+import types
+
+def import_code(code, name):
+    # create blank module
+    module = types.ModuleType(name)
+    # populate the module with code
+    exec(code, module.__dict__)
+    return module
+
+code = """
+def testFunc():
+    print('spam!')
+"""
+
+m = import_code(code, 'test')
+m.testFunc()
